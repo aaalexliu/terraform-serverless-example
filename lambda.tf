42417cb6c954a9b558cb3957cc3ccab2b9d9965b
alexliusq <uilxela7@gmail.com>
2020-06-10 22:20:39 +0800

    wow ok that was verbose. proxies, integration, jesus

--- lambda.tf
+++ lambda.tf
@@ -84,3 +84,43 @@ resource "aws_api_gateway_integration" "lambda" {
   type = "AWS_PROXY"
   uri = aws_lambda_function.example.invoke_arn
 }
+
+# The AWS_PROXY integration type causes API gateway to call into the API of another
+# AWS service. In this case, it will call the AWS Lambda API to create an "invocation"
+# of the Lambda function.
+
+# Unfortunately the proxy resource cannot match an empty path at the root of the API.
+# To handle that, a similar configuration must be applied to the root resource that
+# is built in to the REST API object:
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_rest_api.example.root_resource_id
+  http_method = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.example.invoke_arn
+}
+
+
+
+# Finally, you need to create an API Gateway "deployment" in order to activate the
+# configuration and expose the API at a URL that can be used for testing:
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name = "test"
+}
